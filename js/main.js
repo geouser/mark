c435b174b126297b776e13130e0c909bf3184bb2
Serhiy <serhiyhulyi93@gmail.com>
2016-09-27 23:13:27 +0300

    portfolio start

--- js/main.js
+++ js/main.js
@@ -71,6 +71,17 @@ jQuery(document).ready(function($) {
         arrows: false
     });
 
+    /*----------------------------
+                              Mix
+    -------------------------*/
+    $('.mix-container').mixItUp({
+        animation: {
+            duration: 400,
+            effects: 'fade translateZ(-360px) stagger(34ms)',
+            easing: 'ease'
+        }
+    });
+
 
 
     /*----------------------------
